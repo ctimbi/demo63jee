891d2474f8404909c5ecc85e0dd58104de5a07c2
Cristian Timbi <ctimbi@gmail.com>
2024-01-15 08:26:08 -0500

    Add cors

---
+++ src/main/java/ec/edu/ups/ppw63/demo63/services/CORSFilter.java
@@ -0,0 +1,24 @@
+package ec.edu.ups.ppw63.demo63.services;
+
+
+import java.io.IOException;
+
+import jakarta.ws.rs.container.ContainerRequestContext;
+import jakarta.ws.rs.container.ContainerResponseContext;
+import jakarta.ws.rs.container.ContainerResponseFilter;
+import jakarta.ws.rs.ext.Provider;
+
+@Provider
+public class CORSFilter implements ContainerResponseFilter {
+    @Override
+    public void filter(ContainerRequestContext requestContext, ContainerResponseContext responseContext) throws IOException {
+        responseContext.getHeaders().add("Access-Control-Allow-Origin", "*");
+        responseContext.getHeaders().add("Access-Control-Allow-Headers", "origin, content-type, accept, authorization,access-control-allow-origin,Referrer-Policy");
+        responseContext.getHeaders().add("Access-Control-Allow-Credentials", "true");
+        responseContext.getHeaders().add("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD");
+        
+        responseContext.getHeaders().add("Access-Control-Max-Age", "1209600");
+
+
+    }
+}
